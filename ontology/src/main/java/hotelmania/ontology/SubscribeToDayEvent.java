d0aa1a4fef4538678056a28d063bb5809c9ad1cb
isaac <isaac@Kratos-Pc>
2014-05-07 15:25:48 +0200

    First Commit using Ontology EMSE

---
+++ ontology/src/main/java/hotelmania/ontology/SubscribeToDayEvent.java
@@ -0,0 +1,15 @@
+package hotelmania.ontology;
+
+
+import jade.content.*;
+import jade.util.leap.*;
+import jade.core.*;
+
+/**
+* Protege name: SubscribeToDayEvent
+* @author ontology bean generator
+* @version 2014/05/5, 13:17:33
+*/
+public class SubscribeToDayEvent implements AgentAction {
+
+}
